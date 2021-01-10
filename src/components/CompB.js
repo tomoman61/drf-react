64384af9b1d9de9ed8a15d7f93e6420c81ea6daf
tomoman61 <inotomo5@gmail.com>
2021-01-10 10:20:00 +0900

    CompB.js CompC.js 新規作成

---
+++ src/components/CompB.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompB = () => {
+    return (
+        <></>
+    )
+}
+
+export default CompB
---
+++ src/components/CompC.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompC = () => {
+    return (
+        <></>
+    )
+}
+
+export default CompC
